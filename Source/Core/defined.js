4d6966d2743de14cc2b013e0d685b6185fe78a88
machfe522 <machfe@126.com>
2020-08-16 14:03:45 +0800

    增加vs code调试配置。

--- Source/Core/defined.js
+++ Source/Core/defined.js
@@ -12,6 +12,10 @@
  * }
  */
 function defined(value) {
+  /*
+   == 和 != 比较若类型不同，先偿试转换类型，再作值比较，最后返回值比较结果 。
+   === 和 !== 只有在相同类型下,才会比较其值 。
+  */
   return value !== undefined && value !== null;
 }
 export default defined;
